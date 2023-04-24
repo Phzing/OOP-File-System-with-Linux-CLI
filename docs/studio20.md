a6f8291b2cf4a5315e5851cb079554cd4f9268c9
hoffman.z <hoffman.z@wustl.edu>
2023-04-24 11:53:00 -0500

    studio 20 done

--- docs/studio20.md
+++ docs/studio20.md
@@ -3,3 +3,12 @@
 
 2. It is important that we delete the pointer to the actual file because if we don't delete the pointer, we will 
 have a memory leak upon destruction of the PasswordProxy object.
+
+3. Implemented all
+
+4. Tested reading, writing, and displaying a file via the proxy object. First, had proxy object with no contents. Read and printed
+out results from read. Nothing was printed. Then, wrote a vector into the file and returned the result of the write call (was a 0).
+Read proxy again and printed results which showed the correct vector being printed. Then, created a MetadataDisplayVisitor object and 
+called accept on our proxy, passing the MetadataDisplayVisitor object and saw the contents printed to the terminal ostream.
+
+5. All unit tests pass
--- src/Studio20.cpp
+++ src/Studio20.cpp
@@ -13,16 +13,24 @@ int main(int argc, char* argv[]) {
     cout << "here" << endl;
     AbstractFile *myFile = new ImageFile("aaa");
     //the password for this file will be "password"
-    vector<char> contents = myFile->read();
+    
+    PasswordProxy *proxy = new PasswordProxy(myFile, "password");
+
+    vector<char> contents = proxy->read();
+    cout << "Contents (read 1):";
+    for (int i = 0; i < contents.size(); i++) {
+        cout << contents[i];
+    }
+    cout << endl;
     for (int i = 0; i < 5; ++i) {
         contents.push_back('X');
         contents.push_back(' ');
     }
     contents.push_back('3');
-    myFile->write(contents);
-    PasswordProxy *proxy = new PasswordProxy(myFile, "password");
+    cout << "Proxy write: " << proxy->write(contents) << endl;
+
     contents = proxy->read();
-    cout << "Contents:";
+    cout << "Contents (read 2):";
     for (int i = 0; i < contents.size(); i++) {
         cout << contents[i];
     }
